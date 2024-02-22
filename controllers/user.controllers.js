d9cc307697778eda15fe571602aa0d3cc8acd67c
dasom.kim <dev.dasom1012@gmail.com>
2024-02-22 13:42:18 +0900

    feat(User): update user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,3 +1,5 @@
+import User from '../models/user.model.js';
+
 export const getUser = (req, res, next) => {
   const { user } = req;
   try {
@@ -6,6 +8,28 @@ export const getUser = (req, res, next) => {
       return res.status(200).json({
         ...userInfo,
       });
+    } else {
+      const err = new Error();
+      err.message = 'Not authorized';
+      err.statusCode = 401;
+      throw err;
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const { name, profileUrl } = req.body;
+    const filter = { _id: user._id };
+    const update = { name, profileUrl };
+    if (user) {
+      const updatedUser = await User.findOneAndUpdate(filter, update, {
+        new: true,
+      });
+      return res.status(200).json({ ...updatedUser._doc });
     } else {
       const err = new Error();
       err.message = 'Not authorized';
