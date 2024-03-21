20e860b002e1ebbcc08cbed3cc87b5983c5fad50
Dasom <dev.dasom1012@gmail.com>
2024-03-21 22:21:40 +0900

    feat: Deploy to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,43 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+import functions from 'firebase-functions';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+import {
+  invalidPathHandler,
+  errorResponseHandler,
+} from './middleware/error.middleware.js';
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import sentenceRoutes from './routes/sentence.route.js';
+import bookRoutes from './routes/book.route.js';
+
+dotenv.config();
+
+const app = express();
+// DB연결
+connectDB();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+app.use('/sentence', sentenceRoutes);
+app.use('/book', bookRoutes);
+
+app.use(invalidPathHandler);
+app.use(errorResponseHandler);
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
+// /api prefix를 가지는 요청을 express 라우터로 전달
+const api = functions.region('asia-northeast2').https.onRequest(app);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export { api };
--- functions/middleware/error.middleware.js
+++ functions/middleware/error.middleware.js
@@ -1,12 +1,10 @@
-import { validationResult } from 'express-validator';
-
 export const errorResponseHandler = (error, req, res, next) => {
   if (process.env.NODE_ENV !== 'production') {
     console.log(error);
   }
 
   const statusCode = error.statusCode || 500;
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     message: error.message,
     stack: process.env.NODE_ENV === 'production' ? null : error.stack,
   });
