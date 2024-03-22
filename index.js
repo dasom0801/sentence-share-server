f0127f6b61c3ed28b1e598b302ea598e22ae3877
Dasom <dev.dasom1012@gmail.com>
2024-03-22 09:48:17 +0900

    Revert "Merge pull request #11 from dasom0801/feat/firebase"
    This reverts commit b4497cca7bc2528d5cb08a97c21a7a974bb197f5, reversing
    changes made to 8cf7725c75f3a8bbd812ded590e0b4e199e43d45.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import functions from 'firebase-functions';
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -27,17 +20,17 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
-app.use('/sentence', sentenceRoutes);
-app.use('/book', bookRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/sentence', sentenceRoutes);
+app.use('/api/book', bookRoutes);
+app.get('/health', (req, res, next) => {
+  return res.status(200).json('ok');
+});
 
 app.use(invalidPathHandler);
 app.use(errorResponseHandler);
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-// /api prefix를 가지는 요청을 express 라우터로 전달
-const api = functions.region('asia-northeast2').https.onRequest(app);
-
-export { api };
+app.listen('8080', () => {
+  console.log('Server is listening');
+});
--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,10 +1,12 @@
+import { validationResult } from 'express-validator';
+
 export const errorResponseHandler = (error, req, res, next) => {
   if (process.env.NODE_ENV !== 'production') {
     console.log(error);
   }
 
   const statusCode = error.statusCode || 500;
-  return res.status(statusCode).json({
+  res.status(statusCode).json({
     message: error.message,
     stack: process.env.NODE_ENV === 'production' ? null : error.stack,
   });
