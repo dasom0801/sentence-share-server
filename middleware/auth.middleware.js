443de873cad33a6de43eda8e2b6e156f5b7ced09
Dasom <dev.dasom1012@gmail.com>
2024-07-18 15:46:47 +0900

    오타 수정

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,26 +1,26 @@
 import { getUserFromToken } from '../utils/utils.js';
 
 export const authGuard = async (req, res, next) => {
-  const trhowError = () => {
-    const err = new Error('Not authorized');
-    err.statusCode = 401;
-    next(err);
-  };
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      const user = await getUserFromToken(req);
-      if (!user) {
-        trhowError();
-      }
-      req.user = user;
-      next();
-    } catch (error) {
-      trhowError();
-    }
-  } else {
-    trhowError();
-  }
+	const throwError = () => {
+		const err = new Error('Not authorized');
+		err.statusCode = 401;
+		next(err);
+	};
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		try {
+			const user = await getUserFromToken(req);
+			if (!user) {
+				throwError();
+			}
+			req.user = user;
+			next();
+		} catch (error) {
+			throwError();
+		}
+	} else {
+		throwError();
+	}
 };
