ca4db4a7a8bd814d512505e589c935594de3d28c
Dasom <dev.dasom1012@gmail.com>
2024-12-30 19:02:25 +0900

    fix: 상대 경로로 import 되돌림

--- functions/config/firebase.config.js
+++ functions/config/firebase.config.js
@@ -1,15 +1,6 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-
-import fs from 'fs';
-import path from 'path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const serviceAccountPath = path.resolve(
-  __dirname,
-  './service-account-file.json',
-);
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
+import serviceAccount from './service-account-file.json' assert { type: 'json' };
 
 dotenv.config();
 
